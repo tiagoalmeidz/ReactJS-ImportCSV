a483e3aaa4af3e12f8dc875ed1d68acb5c05c8b5
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-06-21 00:31:32 -0300

    Alterando label

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import CSVReader from "react-csv-reader";
-
-const handleForce = data => {
-  let csv = data;
-  console.log(csv);
-};
 
 const reader = (
   <div className="container">
     <CSVReader
       cssClass="react-csv-input"
-      label="Select CSV with secret Death Star statistics"
-      onFileLoaded={handleForce}
+      label="Selecione um arquivo CSV:"
+      onFileLoaded={data => console.log("arquivos :", data)}
     />
     <p>and then open the console</p>
   </div>
