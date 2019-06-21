9c9dfeb9d5598e49a46bf4a64b3191c4b2bed909
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-06-21 00:33:44 -0300

    Inserindo handlefile

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CSVReader from "react-csv-reader";
 
+const handleFile = data => console.log(data);
+
 const reader = (
   <div className="container">
     <CSVReader
       cssClass="react-csv-input"
       label="Selecione um arquivo CSV:"
-      onFileLoaded={data => console.log("arquivos :", data)}
+      onFileLoaded={handleFile}
     />
     <p>and then open the console</p>
   </div>
